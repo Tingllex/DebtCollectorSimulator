60c410fe97a2c5c7d6aa6ee2fb1164c9e2b630e3
Jakub Zablocki <Tingllex@yahoo.com>
2022-05-15 21:07:12 +0200

    Briefcase huge update

--- Debt Collector/Assets/Scripts/BriefCase.cs
+++ Debt Collector/Assets/Scripts/BriefCase.cs
@@ -5,13 +5,18 @@ using UnityEngine;
 public class BriefCase : MonoBehaviour
 {
     public static int CollectedCash;
+    public Transform target;
+
     private void Start()
     {
         CollectedCash = 0;
     }
     void Update()
     {
-        
+        var dir = target.position - transform.position;
+        Quaternion LookAtRotation = Quaternion.LookRotation(dir);
+        Quaternion OnlyYAxis = Quaternion.Euler(transform.rotation.eulerAngles.x, LookAtRotation.eulerAngles.y + 90, transform.rotation.eulerAngles.z);
+        transform.rotation = OnlyYAxis;
     }
 
     private void OnCollisionEnter(Collision collision)
